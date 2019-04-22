a672e018693833b2b625d4b77d9b413b96d51228
JayJayDee <jindongp@gmail.com>
2019-04-22 19:15:39 +0900

    added more implementation in main_page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter_sample/states/app_state.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -8,16 +10,46 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
 
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      AppState state = ScopedModel.of<AppState>(context);
+      state.loadRamens();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Ramenian App'),
       ),
       body: SafeArea(
-        child: ListView(
-          children: []
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _mainListView(context),
+            Positioned(
+              child: _progressBar(context)
+            )
+          ]
         )
       )
     );
   }
+}
+
+Widget _progressBar(BuildContext context) {
+  AppState state = ScopedModel.of<AppState>(context, rebuildOnChange: true);
+  if (state.loading == true) {
+    return CircularProgressIndicator();
+  }
+  return Center();
+}
+
+Widget _mainListView(BuildContext context) {
+  AppState state = ScopedModel.of<AppState>(context, rebuildOnChange: true);
+  return ListView(
+    children: []
+  );
 }
--- lib/states/app_state.dart
+++ lib/states/app_state.dart
@@ -1,12 +1,26 @@
 import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter_sample/entities/ramen.dart';
+import 'package:flutter_sample/factory.dart';
 
 class AppState extends Model {
   List<Ramen> _ramens;
+  bool _loading;
 
   List<Ramen> get ramens => _ramens;
+  bool get loading => _loading;
 
   AppState() {
     _ramens = List();
+    _loading = false;
+  }
+
+  Future<void> loadRamens() async {
+    _loading = true;
+    notifyListeners();
+
+    await factoryInst().ramensRequester.requestRamens();
+
+    _loading = false;
+    notifyListeners();
   }
 }
